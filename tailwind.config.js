dc582e39b0bd88e20ec111432cc4b0cfad9d1db7
unknown <firdavs.j2006@gmail.com>
2022-03-04 18:47:15 +0500

    basic project's structure formed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,css,js}",
+    "./dist/**/*.{html,css.js}",
+    "./index.html"
+  ],
+  theme: {
+    fontFamily: {
+      'manrope': ['"Manrope"']
+    }
+  },
+  plugins: [],
+}
